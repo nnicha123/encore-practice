343bdd4a60457f3454b87114ac539e0b8ba45ff8
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-09 12:01:29 +0700

    Initial commit of encore tutorial

---
+++ greeting/greeting.ts
@@ -0,0 +1,14 @@
+import { api } from "encore.dev/api";
+
+interface Response {
+    data: string;
+}
+
+export const greeting = api({
+    method: 'GET',
+    path: '/greeting/:name',
+    expose: true
+},
+    async ({ name }: { name: string }): Promise<Response> => {
+        return { data: `Hello ${name}!` }
+    })
